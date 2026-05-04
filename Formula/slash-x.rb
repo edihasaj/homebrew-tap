442620bcee677ffa150bf08598cfad3d45669eea
edihasaj <edihasaj@gmail.com>
2026-05-04 22:27:18 +0200

    Update slash-x v0.9.1

--- Formula/slash-x.rb
+++ Formula/slash-x.rb
@@ -1,16 +1,20 @@
 class SlashX < Formula
   desc "Local X/Twitter CLI for posting, replying, and reading from the command line"
   homepage "https://github.com/edihasaj/slash-x#readme"
-  url "https://github.com/edihasaj/slash-x/releases/download/v0.9.0/slash-x-0.9.0.tar.gz"
-  sha256 "7813ee1ea4d5ae185d5a2113aa1e4e27fb02247df85c3a2b0ee7f0c71fd9cb5f"
+  url "https://github.com/edihasaj/slash-x/releases/download/v0.9.1/slash-x-0.9.1.tar.gz"
+  sha256 "a72335d34add260ab78784bea965335818c129dafbffc9df4ee224e33dd2dff7"
   license "MIT"
-  version "0.9.0"
+  version "0.9.1"
 
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    libexec.install Dir["*"]
+    (bin/"slash").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/dist/cli.js" "$@"
+    EOS
+    chmod 0755, bin/"slash"
   end
 
   test do
